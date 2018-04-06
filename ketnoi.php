9fd22e4c93f8f441b751aebb3aa01a4c65fb44ce
hungdanghyper <hungdanghyper@gmail.com>
2018-04-06 21:06:58 +0700

    connect

--- ketnoi.php
+++ ketnoi.php
@@ -1,8 +1,8 @@
 ﻿<?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database = "loginapp";
+$servername = "sql2.freemysqlhosting.net";
+$username = "sql2231060";
+$password = "Hungtan17101996";
+$database = "sql2231060";
 $conn = new mysqli($servername,$username,$password,$database);
 if($conn->connect_error)
 	die("Not Connected".$conn->connect_error);
