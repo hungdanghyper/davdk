cc23d34ab2cd8720886a6eab470939656c7116a0
hungdanghyper <hungdanghyper@gmail.com>
2018-04-07 10:06:34 +0700

    cc

--- index1.php
+++ index1.php
@@ -4,20 +4,29 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-
+  <link rel="shortcut icon" type="image/png" href="chip.png">
   <title> Ðồ Án LTHT-VÐK </title>
-
-  <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
-  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
   <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-
+  <style>
+  .jumbotron{
+    max-width: 1300px;
+    margin: 0 auto;
+  }
+  .size{
+    font-size: 25px;
+  }
+  .center{
+    max-width: 800px;
+    margin:10px auto;
+  }
+</style>
 </head>
-<style> body{background: #dddddd  } </style> <!--url(anhnen.png)-->
 
 <body >
   <?php
 //Khai báo sử dụng session
+  require'ketnoi.php';
   session_start();
 
 //Khai báo utf-8 để hiển thị được tiếng việt
@@ -26,91 +35,178 @@
 //Xử lý đăng nhập
   if (isset($_POST['dangnhap'])) 
   {    
-     //Kết nối tới database
-    include('ketnoi.php');
 
     //Lấy dữ liệu nhập vào
-    $username = $_POST['txtUsername'];
-    $password = $_POST['txtPassword'];
+    $username = addslashes($_POST['txtUsername']);
+    $password = addslashes($_POST['txtPassword']);
 
     //Kiểm tra đã nhập đủ tên đăng nhập với mật khẩu chưa
     if (!$username || !$password) {
-
-
       header('Location:index1.php');
-
+      exit;
     }
-
-    // mã hóa pasword
-    //$password = md5($password);
-
-    //Kiểm tra tên đăng nhập có tồn tại không
-    $query = mysqli_query("SELECT username, password FROM login WHERE username='$username'");
-    if (mysqli_num_rows($query) == 0) {
+    $sql = "SELECT password FROM login WHERE username='$username'";
+    $result = $conn->query($sql);
+    if($result->num_rows > 0)
+    {
+      while($row = $result->fetch_array())
+      {
+        if($password == $row['password'])
+        {
+          $_SESSION['username'] = $username;
+          header('Location:trangchu.php');
+          die();
+        }
+        else
+        {
+          header('Location:index1.php');
+          exit;
+        }
+      }
+    }
+    else
+    {
       header('Location:index1.php');
+      exit;
     }
-
-    //Lấy mật khẩu trong database ra
-    $row = mysqli_fetch_array($query);
-
-    //So sánh 2 mật khẩu có trùng khớp hay không
-    if ($password != $row['password']) {
-      header('Location:index1.php');
-    }
-    
-     $_SESSION['username'] = $username;
-      header('Location:trangchu.php');
-      die();
-  }
+  }   
   ?>
 
-  <div class="row" style="margin-top: 0px;" >
-    <table width="100%" height="70"> 
-     <tr>
-      <td bgcolor="#d8d5d5" align='center' > <h1> <font color='red' > <b> ĐỒ ÁN LẬP TRÌNH HỆ THỐNG VÀ VI ĐIỀU KHIỂN </b> </font> </h1> </td>
-    </tr>
-  </table>
-  <table  width="100%" height="40">
-   <tr>
-    <td bgcolor="#5c5d5d" align='center' > <font color='white' > <b> HỒ TÁ QUÝ - ĐINH HỮU QUÂN - BÙI VIẾT TOÀN </b> </font> </td>
-  </tr>
-</table>
-<table width=50% align='center' border='2'>
- <tr colsD02="5"> <div class="speed-slow"></div> <h3 align='center'> <font color='blue' > <b> Đăng Nhập Để Điều Khiển Thiết Bị </b> </font> </h3> 
- </tr>
-
-
-</div>
-
-
-<div class="row">
-  <div class="col-xs-2 col-md-4"></div>
-  <div class="col-xs-8 col-md-4">
-    <form action="index.php" method="post">
-      <div class="form-group">
-        <label for="text">Tài Khoản:</label>
-        <input type="username" class="form-control" name="txtUsername" placeholder="Nhập Tài Khoản">
-      </div>
-      <div class="form-group">
-        <label for="pwd">Mật Khẩu: </label>
-        <input type="password" class="form-control" name="txtPassword" placeholder="Nhập Mật Khẩu">
-      </div>
-      <div class="checkbox">
-        <label><input type="checkbox"> Nhớ Mật Khẩu</label>
-      </div>
-      <button type="submit" class="btn btn-default" name="dangnhap">Đăng Nhập</button>
-      <div class="alert alert-danger" >
+  <div class="jumbotron">
+    <h1 class="display-4 text-center">ĐỒ ÁN LẬP TRÌNH HỆ THỐNG VÀ VI ĐIỀU KHIỂN</h1>
+    <p class="display-5 text-xl-center text-primary size"> ĐẶNG BÁ HÙNG - NGUYỄN TIẾN DŨNG - PHAN KIỀU HƯNG</p>
+    <hr class="my-4">
+    <p class="display-5 text-xl-center text-justify size">ĐĂNG NHẬP</p>
+    <div class="w-75 center">
+      <form>
+        <div class="form-group">
+          <label>Username</label>
+          <input type="text" class="form-control" aria-describedby="username" placeholder="Enter Username" name="txtUsername">
+        </div>
+        <div class="form-group">
+          <label for="exampleInputPassword1">Password</label>
+          <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" name="txtPassword">
+        </div>
+        <div class="form-check">
+          <input type="checkbox" class="form-check-input" id="exampleCheck1">
+          <label class="form-check-label" for="exampleCheck1">Check me out</label>
+        </div>
+        <button type="submit" class="btn btn-primary" name="dangnhap">Submit</button>
+        <div class="alert alert-danger" >
        <strong>Tài Khoản Không Tồn Tại!</strong>
      </div>
+      </form>
+    </div>
+  </div>
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
 
 
-   </form>
- </div>
- <div class="col-xs-2 col-md-4"></div>
- <!-- jQuery -->
- <script src="bootstrap/js/jquery.js"></script>
- <!-- Bootstrap Core JavaScript -->
- <script src="bootstrap/js/bootstrap.min.js"></script>
+<html>  
+<head>      
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="shortcut icon" type="image/png" href="chip.png">
+  <title> Ðồ Án LTHT-VÐK </title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+  <style>
+  .jumbotron{
+    max-width: 1300px;
+    margin: 0 auto;
+  }
+  .size{
+    font-size: 25px;
+  }
+  .center{
+    max-width: 800px;
+    margin:10px auto;
+  }
+</style>
+</head>
+
+<body >
+  <?php
+//Khai báo sử dụng session
+  require'ketnoi.php';
+  session_start();
+
+//Khai báo utf-8 để hiển thị được tiếng việt
+  header('Content-Type: text/html; charset=UTF-8');
+
+//Xử lý đăng nhập
+  if (isset($_POST['dangnhap'])) 
+  {    
+
+    //Lấy dữ liệu nhập vào
+    $username = addslashes($_POST['txtUsername']);
+    $password = addslashes($_POST['txtPassword']);
+
+    //Kiểm tra đã nhập đủ tên đăng nhập với mật khẩu chưa
+    if (!$username || !$password) {
+      header('Location:index1.php');
+      exit;
+    }
+    $sql = "SELECT password FROM login WHERE username='$username'";
+    $result = $conn->query($sql);
+    if($result->num_rows > 0)
+    {
+      while($row = $result->fetch_array())
+      {
+        if($password == $row['password'])
+        {
+          $_SESSION['username'] = $username;
+          header('Location:trangchu.php');
+          die();
+        }
+        else
+        {
+          header('Location:index1.php');
+          exit;
+        }
+      }
+    }
+    else
+    {
+      header('Location:index1.php');
+      exit;
+    }
+  }   
+  ?>
+
+  <div class="jumbotron">
+    <h1 class="display-4 text-center">ĐỒ ÁN LẬP TRÌNH HỆ THỐNG VÀ VI ĐIỀU KHIỂN</h1>
+    <p class="display-5 text-xl-center text-primary size"> ĐẶNG BÁ HÙNG - NGUYỄN TIẾN DŨNG - PHAN KIỀU HƯNG</p>
+    <hr class="my-4">
+    <p class="display-5 text-xl-center text-justify size">ĐĂNG NHẬP</p>
+    <div class="w-75 center">
+      <form>
+        <div class="form-group">
+          <label>Username</label>
+          <input type="text" class="form-control" aria-describedby="username" placeholder="Enter Username" name="txtUsername">
+        </div>
+        <div class="form-group">
+          <label for="exampleInputPassword1">Password</label>
+          <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" name="txtPassword">
+        </div>
+        <div class="form-check">
+          <input type="checkbox" class="form-check-input" id="exampleCheck1">
+          <label class="form-check-label" for="exampleCheck1">Check me out</label>
+        </div>
+        <button type="submit" class="btn btn-primary" name="dangnhap">Submit</button>
+        <div class="alert alert-danger" >
+       <strong>Tài Khoản Không Tồn Tại!</strong>
+     </div>
+      </form>
+    </div>
+  </div>
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
 </html>
 
