99b4786b285c4d7f04f2e8bdff6d988f80a8ad6a
Manh Hung <hungdanghyper@gmail.com>
2018-05-25 15:03:14 +0700

    hh

--- ketnoi.php
+++ ketnoi.php
@@ -2,7 +2,7 @@
 $servername = "sql9.freemysqlhosting.net";
 $username = "sql9239770";
 $password = "d6efQc4CkN";
-$database = "login";
+$database = "sql9239770";
 $conn = new mysqli($servername,$username,$password,$database);
 if($conn->connect_error)
 	die("Not Connected ".$conn->connect_error);
