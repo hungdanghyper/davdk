94286497fa0e5098a3e43ec567e524423b25eabd
Manh Hung <hungdanghyper@gmail.com>
2018-05-25 15:00:17 +0700

    rl

--- ketnoi.php
+++ ketnoi.php
@@ -1,12 +1,9 @@
 ﻿<?php
-/*$servername = "sql9.freemysqlhosting.net";
+$servername = "sql9.freemysqlhosting.net";
 $username = "sql9239770";
-$password = "d6efQc4CkN";*/
-$servername = "localhost";
-$username = "njgvxtrx";
-$password = "Y158w6tvkB";
-$database = "njgvxtrx_login";
+$password = "d6efQc4CkN";
+$database = "login";
 $conn = new mysqli($servername,$username,$password,$database);
 if($conn->connect_error)
-	die("Not Connected".$conn->connect_error);
+	die("Not Connected ".$conn->connect_error);
 ?>
