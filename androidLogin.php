c90e7dd16ef907ce555e9173e63307fd171e10b8
Manh Hung <hungdanghyper@gmail.com>
2018-05-29 17:54:12 +0700

    ff

--- androidLogin.php
+++ androidLogin.php
@@ -5,16 +5,17 @@ if(isset($_POST['user'])&&isset($_POST['pass'])){
 	//echo $username." - ".$password;
 	require'ketnoi.php';
 	$sql = "SELECT username, password FROM login WHERE username='$username'";
-	if($conn->query($sql) < 0){
-		echo "Username doesn't exist ";
-		exit();
+	$result = $conn->query($sql);
+
+	if ($result->num_rows > 0) {
+    // output data of each row
+		while($row = $result->fetch_assoc()) {
+			echo "OK";
+			exit();
+		}
+	} else {
+		echo "0 results";
 	}
-		$res = $conn->query($sql);
-		$row = $res->fetch_assoc();
-		if ($password == $row['password']) {
-				echo "OK";
-			    exit();
-			}
 	$conn->close();
 }
 ?>
