f8fe09bd64a8a25a0dff8a96cad54e6bddf4e387
sector <hungdbqh1710@gmail.com>
2018-06-25 22:36:04 +0700

    xxx

--- ketnoi.php
+++
@@ -1,9 +0,0 @@
-﻿<?php
-$servername = "sql9.freemysqlhosting.net";
-$username = "sql9239770";
-$password = "d6efQc4CkN";
-$database = "sql9239770";
-$conn = new mysqli($servername,$username,$password,$database);
-if($conn->connect_error)
-	die("Not Connected ".$conn->connect_error);
-?>
